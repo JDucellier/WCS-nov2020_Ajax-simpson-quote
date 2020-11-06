7bea9932664d9d2d12a5c8afa9c58ad055323127
JDucellier <jeanducellier.dev@gmail.com>
2020-11-06 18:31:31 +0100

    resolve probleme

---
+++ index.js
@@ -0,0 +1,29 @@
+// This function loads simpson quotes data from the simpson API
+function fetchSimpsonJSON() {
+
+  let url = `https://simpsons-quotes-api.herokuapp.com/quotes?count=3`;
+  axios.get(url)
+    .then(function(response) {
+      return response.data; // response.data instead of response.json() with fetch
+    })
+    .then(function(simpsons) {
+      console.log('data decoded from JSON:', simpsons);
+
+      let  simpsonHtml = simpsons.map(simpson => 
+        `
+        <div class="card">
+          <img class="img" src="${simpson.image}" />
+          <div class="card-body">
+            <p><strong>${simpson.character}</strong></p>
+            <p>${simpson.quote}</p>
+          </div>
+        </div>
+      `
+      );
+
+      // Build a block of HTML
+      document.querySelector('#simpson').innerHTML = simpsonHtml.reduce((acc, currentValue) => acc + currentValue);
+    });
+}
+
+fetchSimpsonJSON();
